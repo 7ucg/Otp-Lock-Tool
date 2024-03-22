b36fb52ced03090aa89cd3419403a59f0162bd19
7ucg <161209670+7ucg@users.noreply.github.com>
2024-03-22 13:28:09 +0100

    update

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-function startSpam() {
-    const ddi = document.getElementById('country-code').value;
-    const number = document.getElementById('number').value;
-    const phoneNumber = ddi + number;
+  function stARTSPAm	 (		)	 {
+	const DdI  = document .getElementById ('country-code'	)  .value		;
+  const NUmber	 = document	 .getElementById  ('number'	 )		.value		;
+  const phOnENumBEr = DdI + NUmber		;
     
-    fetch('/start-spam', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ phoneNumber, ddi, number })
-    })
-    .then(response => {
-      if (response.ok) {
-        console.log('Spam started successfully.');
-      } else {
-        console.error('Failed to start spam.');
-      }
-    })
-    .catch(error => {
-      console.error('Error occurred while starting spam:', error);
-    });
-  }
+	fetch	 ('/start-spam'  ,	 {
+	 method : 'POST'  ,
+ headers	 :		{
+	'Content-Type' : 'application/json'	}	 ,
+  body		: JSON	 .stringify	 ({ phOnENumBEr		, DdI  , NUmber  } ) }		)	.then	(response  => {
+	 if (response	 .ok	 )  {
+	console .log ('Spam started successfully.'		)	 ;		} else	{
+		console .error		('Failed to start spam.'	 ) ; } }	) .catch	 (error	=>  {
+		console	 .error	 ('Error occurred while starting spam:'	, error		)  ;  }  )	 ;		}
   
